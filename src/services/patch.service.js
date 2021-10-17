da6fbd63a669ad9cc6c81b4a54bb15613156a61b
addegbenga <addegbenga@gmail.com>
2021-10-16 21:07:11 -0400

    added json patch

--- src/services/patch.service.js
+++ src/services/patch.service.js
@@ -2,6 +2,9 @@ const jsonPatch = require('fast-json-patch');
 
 /**
  * patch a json request
+ * @param {object} jsonBody
+ * @param {object} patchBody
+ * @returns {Promise<User>}
  */
 const patchRequest = async (jsonBody, patchBody) => {
   const document = jsonPatch.applyOperation(jsonBody, patchBody).newDocument;
